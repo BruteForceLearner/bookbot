99fd356648427870f9b90415df8c00f3254219e8
david_schilmoeller <dschilmoeller@gmail.com>
2025-11-26 12:15:39 -0800

    look, ma, no seerstone *blech*

--- stats.py
+++ stats.py
@@ -1,4 +1,14 @@
-def word_count(text):
+def get_num_words(text):
     count = len(text.split())
     return count
 
+#?
+def get_chars_dict(text):
+    counts = {}
+    for character in text.lower():
+        if character in counts:
+            counts[character] = counts[character] + 1
+        else: 
+            counts[character] = 1
+    return counts
+#?
