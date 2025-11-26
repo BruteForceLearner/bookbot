d7bf09c5c611a487806ab94e6351cac38df4757e
david_schilmoeller <dschilmoeller@gmail.com>
2025-11-25 18:42:05 -0800

    B: word counter added

---
+++ main.py
@@ -0,0 +1,21 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_count(text)
+    print(f"Found {num_words} total words")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def word_count(text):
+    count = len(text.split())
+    return count
+
+
+main()
+
+
+
